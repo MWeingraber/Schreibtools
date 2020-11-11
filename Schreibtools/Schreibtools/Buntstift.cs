b75f7bf717718e42c891b77525f4f45dd5cb2c73
Moritz Weingraber <weingraber.m03@htlwienwest.at>
2020-11-11 11:40:00 +0100

    Commit

---
+++ Schreibtools/Schreibtools/Buntstift.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Schreibtools
+{
+    class Buntstift : Stift
+    {
+        public Buntstift(double strichstaerke, ConsoleColor farbe) : base(strichstaerke, farbe)
+        {
+            Typ = "Buntstift";
+        }
+    }
+
+}
